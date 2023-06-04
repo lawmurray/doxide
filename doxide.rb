db76fec793cd8aa9a14cd89cec2d5010ea0f1067
Lawrence Murray <lawrence@indii.org>
2023-06-04 13:41:03 -0700

    Updated source URL in Homebrew package.

--- doxide.rb
+++ doxide.rb
@@ -1,7 +1,7 @@
 class Doxide < Formula
   desc "Modern documentation for modern C++. Configure with YAML, output Markdown."
   homepage "https://doxide.org"
-  url "https://download.indii.org/homebrew/doxide-0.0.0.tar.gz"
+  url "https://download.indii.org/brew/doxide-0.0.0.tar.gz"
   version "0.0.0"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "Apache-2.0"
