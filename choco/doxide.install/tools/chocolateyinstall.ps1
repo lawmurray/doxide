e717182ef5fa3d96734e5c933d64ee1af88bfb36
Lawrence Murray <lawrence@indii.org>
2023-08-07 18:55:23 -0700

    Removed unnecessary fields in Chocolatey package files. Minor fixes.

--- choco/doxide.install/tools/chocolateyinstall.ps1
+++ choco/doxide.install/tools/chocolateyinstall.ps1
@@ -5,16 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  file          = ''
   file64        = "$toolsDir\doxide-installer.exe"
-  url           = ''
-  url64bit      = 'https://download.indii.org/win/doxide-installer.exe'
   softwareName  = 'doxide.install'
-  checksum      = ''
-  checksumType  = ''
   checksum64    = 'E578BC535C7E330BEFC646FFCBDA7F3536A6ABC223955F9ECF6319BB85346CCD'
   checksumType64= 'sha256'
-  silentArgs   = '/S'
+  silentArgs   = '/S /DADD_TO_PATH_ALL_USERS=1'
 }
 
 Install-ChocolateyInstallPackage @packageArgs
