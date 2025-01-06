f801f87b16d79efb5acb7733cb7fd56604f048ca
Lawrence Murray <lawrence@indii.org>
2025-01-06 14:31:59 +0700

    Rename headerings "Typedefs and Type Aliases" to just "Type Aliases"

--- src/MarkdownGenerator.cpp
+++ src/MarkdownGenerator.cpp
@@ -144,7 +144,7 @@ void MarkdownGenerator::generate(const std::filesystem::path& output,
         entity.type == EntityType::NAMESPACE ||
         entity.type == EntityType::GROUP);
     if (typedefs.size() > 0) {
-      out << "## Typedefs and Type Aliases" << std::endl;
+      out << "## Type Aliases" << std::endl;
       out << std::endl;
       out << "| Name | Description |" << std::endl;
       out << "| ---- | ----------- |" << std::endl;
@@ -243,7 +243,7 @@ void MarkdownGenerator::generate(const std::filesystem::path& output,
 
     /* detailed descriptions */
     if (typedefs.size() > 0) {
-      out << "## Typedef and Type Alias Details" << std::endl;
+      out << "## Type Alias Details" << std::endl;
       out << std::endl;
       for (auto& child : typedefs) {
         out << "### " << child->name;
